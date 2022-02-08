72d66aea046f487a2940ff669958199e6a71f057
Qweder93 <nikolai.siedov@gmail.com>
2022-02-08 22:03:07 +0200

    transfer_native fix

--- client_test.go
+++ client_test.go
@@ -14,8 +14,8 @@ func TestNew(t *testing.T) {
 	})
 
 	resp, err := client.Auth(context.Background(), venly.AuthRequest{
-		ClientID:     "Testaccount-capsule",
-		ClientSecret: "82c19251-1753-44f5-ae76-93438d3628de",
+		ClientID:     "Amuzed-capsule",
+		ClientSecret: "1e5fe27a-c0c7-4b59-856c-5dd519723e20",
 		GrantType:    "client_credentials",
 	})
 	if err != nil {
@@ -40,7 +40,7 @@ func TestNew(t *testing.T) {
 	println(rr.Result.ID)
 
 	r2, err := client2.Signatures(context.Background(), resp.AccessToken, venly.SignaturesRequest{
-		Pincode:          "14328",
+		Pincode: "14328",
 		SignatureRequest: venly.SignatureRequest{
 			Type:       "MESSAGE",
 			SecretType: "ETHEREUM",
@@ -52,4 +52,32 @@ func TestNew(t *testing.T) {
 		t.Fatal(err)
 	}
 	println(r2.Result.Type)
+
+	dd, err := client.RetrieveWalletBalance(context.Background(), resp.AccessToken, venly.RetrieveWalletRequest{WalletID: "aed10c77-a7a5-4260-b865-7ade0c63a300"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	println(dd.Result.Balance)
+
+	tr, err := client.TransferNative(context.Background(), resp.AccessToken, venly.TransferNativeRequest{
+		TransactionRequest: venly.TxRequest{
+			Type:       "TRANSFER",
+			WalletID:   "aed10c77-a7a5-4260-b865-7ade0c63a300",
+			To:         "0x2346b33F2E379dDA22b2563B009382a0Fc9aA926",
+			SecretType: "ETHEREUM",
+			Value:      0.1,
+			Data:       "test",
+		},
+		Pincode: "1489",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	println(tr.Result.TransactionHash)
+
+	dd, err = client.RetrieveWalletBalance(context.Background(), resp.AccessToken, venly.RetrieveWalletRequest{WalletID: "aed10c77-a7a5-4260-b865-7ade0c63a300"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	println(dd.Result.Balance)
 }
